16a6fe8187ae7a29cbf0a3506d6a9a377a0936fa
lizfcordeiro <lizcordeiro@outlook.com>
2025-07-30 17:59:23 -0300

    Metade da implementação do serviço

--- src/main/java/com/fiap/orderhub/orderhub_pedido_service/domain/StatusPedido.java
+++ src/main/java/com/fiap/orderhub/orderhub_pedido_service/domain/StatusPedido.java
@@ -3,8 +3,5 @@ package com.fiap.orderhub.orderhub_pedido_service.domain;
 public enum StatusPedido {
     PENDENTE,
     CONFIRMADO,
-    FALHA_PAGAMENTO,
-    ENTREGUE,
-    EM_PREPARACAO,
-    EM_TRANSPORTE;
+    FALHA_PAGAMENTO;
 }
