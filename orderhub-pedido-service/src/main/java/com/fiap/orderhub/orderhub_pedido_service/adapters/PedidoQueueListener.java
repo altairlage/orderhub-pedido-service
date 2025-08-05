d3297bf38b582d6cf54171b759cedbccaa06eb32
Gaddini <ferreiragaddini@bol.com.br>
2025-08-05 03:40:00 -0300

    Ajustes de Listener da fila e no initial_setup

--- orderhub-pedido-service/src/main/java/com/fiap/orderhub/orderhub_pedido_service/adapters/PedidoQueueListener.java
+++ orderhub-pedido-service/src/main/java/com/fiap/orderhub/orderhub_pedido_service/adapters/PedidoQueueListener.java
@@ -3,6 +3,9 @@ package com.fiap.orderhub.orderhub_pedido_service.adapters;
 import br.com.orderhub.core.dto.pedidos.CriarPedidoDTO;
 import com.fiap.orderhub.orderhub_pedido_service.service.OrquestradorCriacaoPedido;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,5 +21,10 @@ public class PedidoQueueListener {
 
     public void consumir(CriarPedidoDTO dto) {
         criacaoPedido.criarPedido(dto);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
